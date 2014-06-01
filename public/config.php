c9c3c0d23ddf549033640fa80694e935e23d32b6
David Asabina <david@supr.nu>
2014-06-01 15:46:42 +0200

    Completed the setup + cleared paths and credentials from source

---
+++ public/config.php
@@ -0,0 +1,29 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://' + getenv("HOST") + '/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://' + getenv("HOST") + '/');
+echo(HTTP_SERVER);
+echo(HTTPS_SERVER);
+
+// DIR
+define('DIR_APPLICATION', getenv("BASE") + '/public/catalog/');
+define('DIR_SYSTEM', getenv("BASE") + '/public/system/');
+define('DIR_LANGUAGE', getenv("BASE") + '/public/catalog/language/');
+define('DIR_TEMPLATE', getenv("BASE") + '/public/catalog/view/theme/');
+define('DIR_CONFIG', getenv("BASE") + '/public/system/config/');
+define('DIR_IMAGE', getenv("BASE") + '/public/image/');
+define('DIR_CACHE', getenv("BASE") + '/public/system/cache/');
+define('DIR_DOWNLOAD', getenv("BASE") + '/public/system/download/');
+define('DIR_MODIFICATION', getenv("BASE") + '/public/system/modification/');
+define('DIR_LOGS', getenv("BASE") + '/logs/');
+
+// DB
+$url=parse_url(getenv("DATABASE_URL"));
+define('DB_DRIVER', 'mpdo');
+define('DB_HOSTNAME', $url["host"]);
+define('DB_USERNAME', $url["user"]);
+define('DB_PASSWORD', $url["pass"]);
+define('DB_DATABASE', substr($url["path"],1));
+define('DB_PREFIX', 'oc_');
